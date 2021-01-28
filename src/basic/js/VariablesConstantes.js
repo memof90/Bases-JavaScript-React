32694a54a0c08339f599c8bbe2ce181b97ba4d39
memof90 <memof80@gmail.com>
2021-01-28 16:37:29 -0500

    initial setup

---
+++ src/basic/js/VariablesConstantes.js
@@ -0,0 +1,11 @@
+
+/* Varibles y constantes */
+
+// var cambia a let 
+let apellido = 'Figueredo';
+apellido = 'Guitierez'
+// let cambia a const 
+const nombre = 'Memo';
+
+
+console.log(apellido, nombre);
