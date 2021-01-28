7b538a5cbb6870f2a70fcb832ca731f7432770c1
memof90 <memof80@gmail.com>
2021-01-28 16:52:16 -0500

    Strings literals

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@
 /* Templates String */
 console.log('hello')
 
-// concatenar
-const names = 'maria';
-const lastaname = 'martinez';
-
-const fullName = 'hola' + ' ' + names + lastaname;
-
-console.log(fullName);
-
-
-const fullNameTwo = `hola ${names} ${lastaname}`
-
-console.log(fullNameTwo)
